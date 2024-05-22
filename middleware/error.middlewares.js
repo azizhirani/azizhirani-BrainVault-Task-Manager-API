e89bf0b57acdd19314e585df7dbd050953daa923
Aziz  Hirani <Azizmh02hirani@gmail.com>
2024-05-22 13:38:26 +0530

    First Commit

---
+++ middleware/error.middlewares.js
@@ -0,0 +1,21 @@
+
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ message: err.message });
+  }
+  
+  const status = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(status);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : '🥞',
+    isError: true
+  });
+};
+
+export {
+  errorHandler
+}
